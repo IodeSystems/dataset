60aa55a73e0719a1139433a321afe4475edec7ce
Carl Taylor <carl@etaylor.me>
2025-05-07 16:24:55 -0700

    Prepare next development iteration: 7.0.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import java.io.InputStreamReader
 import java.time.Duration
 
 group = "com.iodesystems.dataset"
-version = "7.0.0"
+version = "7.0.1-SNAPSHOT"
 description =
   "dataset is a simple query language parser that converts user queries to SQL conditions (using Antlr4 and JOOQ) with an aim for least surprise."
 
