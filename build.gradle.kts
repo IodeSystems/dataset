86a86a176d307ada25213234e58f61caacf85f20
Carl Taylor <carl@etaylor.me>
2025-08-15 12:10:31 -0700

    Release 8.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.time.Duration
 
 group = "com.iodesystems.dataset"
-version = "8.0.0-SNAPSHOT"
+version = "8.0.0"
 description =
   "dataset is a simple query language parser that converts user queries to SQL conditions (using Antlr4 and JOOQ) with an aim for least surprise."
 
